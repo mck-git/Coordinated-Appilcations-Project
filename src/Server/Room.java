13728b079eb6b70419e4217daf0d51712874d05a
Jenscarl Eschen <jenscarlwow@gmail.com>
2018-01-04 15:48:14 +0100

    Worked on server

---
+++ src/Server/Room.java
@@ -0,0 +1,18 @@
+package Server;
+
+import org.jspace.SequentialSpace;
+import org.jspace.Space;
+
+public class Room extends SequentialSpace
+{
+    private String name;
+    private String owner;
+
+
+    public Room (String name, String owner)
+    {
+        this.name = name;
+        this.owner = owner;
+    }
+
+}
--- src/Server/Server.java
+++ src/Server/Server.java
@@ -4,12 +4,22 @@ import org.jspace.*;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.List;
+
+// Templates
+// client template: ("user", string name)
+// room template: ("room", string name, string owner)
+// createRoom template: ("createRoom", string name, string owner)
+
 
 public class Server {
+    static Space lounge = new SequentialSpace();
+    static ArrayList<Room> rooms = new ArrayList<Room>();
 
     public static void main(String[] args)
     {
-        Space lounge = new SequentialSpace();
+
 
         String localhostAddress = null;
         try {
@@ -22,6 +32,34 @@ public class Server {
         repository.addGate("tcp://"+localhostAddress+":9002/?keep");
         repository.add("lounge", lounge);
 
+        try {
+            // Setup registration_lock
+            lounge.put("__registration_lock");
+
+            while (true)
+            {
+                List<Object[]> users = lounge.queryAll(new ActualField("user"), new FormalField(String.class));
+                for (Object[] o : users)
+                {
+                    System.out.println(o[1]);
+                }
+
+                Thread.sleep(1000);
+            }
+        } catch (Exception e) { e.printStackTrace(); }
+
 
     }
+
+    private static boolean createRoom(String name, String owner)
+    {
+        // Check unique
+        boolean taken = false;
+
+
+
+
+    }
+
+
 }
