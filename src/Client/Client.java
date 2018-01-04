c7da7182e2adb71218b02ef4a0ec7cc673fd9d0c
s154666 <s154666@student.dtu.dk>
2018-01-04 13:47:07 +0100

    Added general client for connecting to server

---
+++ src/Client/Client.java
@@ -0,0 +1,34 @@
+package Client;
+
+import org.jspace.*;
+
+import java.util.Scanner;
+
+public class Client {
+
+    public static void main(String[] args)
+    {
+        RemoteSpace room = new RemoteSpace("tcp://CHANGE ME");
+
+//        Scanner sc = new Scanner(System.in);
+//
+//        System.out.print("Please type your username: ");
+//
+//        String usr = sc.next();
+//
+//        room.put(usr);
+//
+//
+//        while(true)
+//        {
+//            if(sc.hasNext())
+//            {
+//                room.put(sc.next(),usr);
+//            }
+//
+//
+//        }
+
+    }
+
+}
